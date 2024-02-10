65add7c94ac30b69c7096d4e0cd10160958358bb
marinn999 <marinacompel@icloud.com>
2024-02-11 00:27:48 +0100

    to check

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,15 @@
-function getElementWidth (content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
-    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-    return totalWidth;
+function filterArray(numbers, value) {
+  let array = [];
+  for (const element of numbers) {
+    if (element > value) {
+      array.push(element);
+    }
+  }
+  return array;
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
